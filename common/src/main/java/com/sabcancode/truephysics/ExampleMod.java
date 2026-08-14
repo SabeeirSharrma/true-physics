9dcd920d6b75603763386128ac7cd5e550c05cca
SabeeirSharrma <sabplayzthepro@gmail.com>
2026-08-14 17:57:01 +0530

    Replace fabric with architecture template

---
+++ common/src/main/java/com/sabcancode/truephysics/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.sabcancode.truephysics;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "true_physics";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
